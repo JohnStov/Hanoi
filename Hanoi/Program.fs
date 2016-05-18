b985b69ead387da53ad5420c05cd19d486a1f6c6
John Stovin <john.d.stovin@gmail.com>
2016-05-18 16:56:18 +0100

    Final game

--- Hanoi/Program.fs
+++ Hanoi/Program.fs
@@ -1,31 +1,53 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-type Ring = int
-
 type Post = int list
 
 type Game = Post list
-
-let init i =
-    [ [ 1 .. i ]; []; [] ]
 
 let display (game : Game) = 
     let displayPost (post : Post) =
         printfn "|%s" (post |> List.fold (fun acc elem -> elem.ToString() + acc) "")
     game |> List.iter displayPost
+    printfn ""
+    game
 
-let removeTop postNumber (game : Game) =
-    let removeTop (post : Post) = 
+let init i =
+    [ [ 1 .. i ]; []; [] ] |> display
+
+let top postNumber (game : Game) =
+    if game.Length <= postNumber 
+    then None 
+    else match game.[postNumber] with
+         | [] -> None
+         | h :: t -> Some h
+
+let pop postNumber (game : Game) =
+    let pop (post : Post) = 
         match post with
-        | [] -> (None, post)
-        | h::t -> Some(h), t
-    let top, newPost = removeTop game.[postNumber]
-    let newGame = game |> List.mapi (fn i elem -> 
+        | [] -> post
+        | h::t -> t
+    game |> List.mapi (fun i elem -> if i = postNumber then pop elem else elem)
+
+let push (ringNumber : int Option) postNumber (game : Game) =
+    let push ringNumber (post : Post) =
+        match ringNumber with
+        | Some n -> n :: post
+        | None -> post 
+    game |> List.mapi (fun i elem -> if i = postNumber then push ringNumber elem else elem)
+
+let moveTop fromPost toPost (game : Game) =
+    let ring = game |> top fromPost
+    game |> pop fromPost |> push ring toPost |> display
+
+let rec moveStack fromPost toPost viaPost height (game : Game) =
+    match height with
+    | 0 -> game
+    | 1 -> game |> moveTop fromPost toPost
+    | _ -> game |> moveStack fromPost viaPost toPost (height - 1) |> moveTop fromPost toPost |> moveStack viaPost toPost fromPost (height - 1)
 
 [<EntryPoint>]
 let main argv = 
-    let game = init 5
-    display game
+    ignore (init 9 |> moveStack 0 1 2 9)
 
     0 // return an integer exit code
